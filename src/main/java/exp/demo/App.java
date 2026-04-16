3ccbf57009993762d846066a7a235d243ef03762
DEEKSHAKR <82014392+DEEKSHAKR@users.noreply.github.com>
2026-04-16 09:38:07 +0530

    Update App.java

--- src/main/java/exp/demo/App.java
+++ src/main/java/exp/demo/App.java
@@ -10,7 +10,7 @@ return a + b;
 public static void main(String[] args) {
 App app=new App();
 int result =app.add(2,3);
-system.out.println("2+3=" + result);
-system.out.println("applicationn executed successfuly!");
+System.out.println("2+3=" + result);
+System.out.println("applicationn executed successfuly!");
 }
 }
